0a66a9f56f662318abab976d8e6734b2016daa66
cat_or_not <41955154+catornot@users.noreply.github.com>
2022-08-27 14:27:26 -0400

    client js code clean up

--- web_src/upload.js
+++ web_src/upload.js
@@ -12,13 +12,11 @@ window.addEventListener( "load", () => {
             alert( "Your Brower doesn't support the File API" );
             return
         }
-
     
         const files = f.files;
-        // console.log( files );
 
         for( var i = 0; i < files.length ; i++ ) {
-            if ( !check_multifile_logo( files[i] ) ) { continue }
+            if ( !check_file_format( files[i] ) ) { continue }
             if ( !files[i].type.match( "image" ) ) { continue }
 
             const picReader = new FileReader();
@@ -45,11 +43,7 @@ window.addEventListener( "load", () => {
 
                     let length = base64chunk.length
 
-                    // if ( typeof( chunk ) != String ) {
-                    //     length = chunk.byteLength
-                    // }
-
-                    const response = await fetch ( "http://localhost:7878/upload", {
+                    await fetch ( "http://localhost:7878/upload", {
                         "method": "POST-IMAGE",
                         "headers": {
                             "length": length,
@@ -58,58 +52,18 @@ window.addEventListener( "load", () => {
                         },
                     } )
 
-                    // console.log( response.status )
-
                     label.innerHTML = `${ parseInt( chunkId * CHUNK_SIZE / parseInt( picFile.result.byteLength ) * 100 ) }%`;
                 }
 
-                console.log( picFile.result.byteLength );
-
                 label.innerHTML = `${ 100 }%`;
             };
-
-            // console.log( files[i] );
             
             picReader.readAsArrayBuffer( files[i] );
         }
     } )
 } )
 
-// $('#multi_file_upload').change(function(e) {
-//     var file_id = e.target.id;
-
-//     var file_name_arr = new Array();
-//     var process_path = site_url + 'public/uploads/';
-
-//     for (i = 0; i < $("#" + file_id).prop("files").length; i++) {
-
-//         var form_data = new FormData();
-//         var file_data = $("#" + file_id).prop("files")[i];
-//         form_data.append("file_name", file_data);
-
-//         if (check_multifile_logo($("#" + file_id).prop("files")[i]['name'])) {
-//             $.ajax({
-//                 //url         :   site_url + "inc/upload_image.php?width=96&height=60&show_small=1",
-//                 url: site_url + "inc/upload_contact_info.php",
-//                 cache: false,
-//                 contentType: false,
-//                 processData: false,
-//                 async: false,
-//                 data: form_data,
-//                 type: 'post',
-//                 success: function(data) {
-//                     // display image
-//                 }
-//             });
-//         } else {
-//             $("#" + html_div).html('');
-//             alert('We only accept JPG, JPEG, PNG, GIF and BMP files');
-//         }
-
-//     }
-// });
-
-function check_multifile_logo(file) {
+function check_file_format(file) {
     let extension = file.type
     if (extension === 'image/jpg' || extension === 'image/jpeg' || extension === 'image/png' ) {
         return true;
